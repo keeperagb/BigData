b351f592049a1d1413b87c3423ac7bf7c7266669
keeperagb <alisson_gean18@hotmail.com>
2022-05-21 15:58:00 -0400

    Imagens
    Salvo Imagens do trabalho

--- Producao de Campos de Soja.R
+++ Producao de Campos de Soja.R
@@ -32,12 +32,51 @@ glimpse(Producao) #consulta dados da planilha
 
 ###################################################################################
 
+#---------------------------------------#---------------------------------------#---------------------------------------#
 #Filtramos a Produção somente da Soja que tem o nome cientifico de "Glycine max (L.) Merr." atrbuindo a uma nova Variavel Soja
 soja <- Producao %>% filter(Especie == 'Glycine max (L.) Merr.')
 
-#Criamos uma nova Variavel de soja com agrupamento por UF
-sojaEstado <-
-  Producao %>% group_by(UF)
+sojaBrasil2122 <- soja %>% filter(Safra == '2021/2022')
+sojaSinop2122 <- soja %>% filter(Municipio == 'Sinop' & Safra == '2021/2022')
+sojaSorriso2122 <- soja %>% filter(Municipio == 'Sorriso' & Safra == '2021/2022')
+sojaMatoGrosso2122 <- soja %>% filter(UF == 'MT' & Safra == '2021/2022')
+#---------------------------------------#---------------------------------------#---------------------------------------#
 
+#Producao de campos de SEMENTE de soja em comparação com as areas dos estados do Brasil
+ggplot(soja, aes(x = UF, y = Area))+geom_col()+scale_y_continuous(n.breaks = 40, expand = expansion(add = c(0, 5000000)))
+#Imagem: Produção Nacional Area plantada x UF
 
+#Producao de campos de SEMENTE de soja em comparação com as areas dos estados do Brasil safra 2021/2022
+ggplot(sojaBrasil2122, aes(x = UF, y = Area))+geom_col()+scale_y_continuous(n.breaks = 40, expand = expansion(add = c(0, 5000000)))
+#Imagem: Produção Nacional Area plantada x UF
 
+#---------------------------------------#---------------------------------------#---------------------------------------#
+#Produção de campos de semente de soja, no Brasil, classificados por area de cada categoria plantada.
+ggplot(soja, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 5000000)))
+
+#Produção de campos de semente de soja, no Brasil na safra de 2021/2022, classificados por area de cada categoria plantada.
+ggplot(sojaBrasil2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 10, expand = expansion(add = c(0, 500000)))
+#Imagem: Producao Campo semente soja safra 21 22 Brasil
+
+#Produção de campos de semente de soja, no estado do MT na safra de 2021/2022, classificados por area de cada categoria plantada.
+ggplot(sojaMatoGrosso2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500000)))
+gMt <- ggplot(sojaMatoGrosso2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500000)))
+#Imagem: Producao campo semente soja safra 21 22 Mato Grosso
+
+#Produção de campos de semente de soja, na cidade de Sinop na safra de 2021/2022, classificados por area de cada categoria plantada.
+ggplot(sojaSinop2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500)))
+gSinop <- ggplot(sojaSinop2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500)))
+#Imagem: Producao campo semente soja 21 22 Sinop MT
+
+#---------------------------------------#---------------------------------------#---------------------------------------#
+#Grafico verificando a produção de cada Cultivar na cidade de Sinop na safra 2021/2022 separadas por classes
+gSinop+facet_grid( ~ Cultivar)
+#Imagem: Categorias Semente Soja Plantadas SINOP safra 21 22
+
+#Grafico verificando a produção de cada Cultivar na cidade de Sinop na safra 2021/2022 separadas por classes e status
+gSinop+facet_grid(Status ~ Cultivar)
+#Imagem: Cultivares Status Sinop safra 21 22
+
+#Verificando sobre o estato do Mato Grosso
+gMt+facet_grid( ~ Status)
+#---------------------------------------#---------------------------------------#---------------------------------------#
