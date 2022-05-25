fc1d8d25a253b4abf4b6d2ce4c32777801acab4d
keeperagb <alisson_gean18@hotmail.com>
2022-05-24 22:21:41 -0400

    Mudanças de Info
    Algumas mudanças nas informações e alinhamentos tomados para finalização do trabalho.

--- Producao de Campos de Soja.R
+++ Producao de Campos de Soja.R
@@ -1,11 +1,10 @@
 #Instalando pacotes
 
-install.packages("tidyverse")
-install.packages("dplyr")
-install.packages("csv")
-install.packages("xlsx")
-install.packages("tidyverse")
-install.packages("ggplot2")
+install.packages("tidyverse") #Pacote com varios componente que é composto tais como dplyr, ggplot2.
+install.packages("dplyr") #Conjunto de funções projetado para permitir a manipulação de dataframe.
+install.packages("csv") #Pacote de leitura de arquivo CSV.
+install.packages("xlsx") #Pacote de leitura de arquivo CSV.
+install.packages("ggplot2") #Pacote de visualização de dados em graficos.
 
 #Carregamento da bibliotecas
 library(csv)
@@ -42,41 +41,28 @@ sojaSorriso2122 <- soja %>% filter(Municipio == 'Sorriso' & Safra == '2021/2022'
 sojaMatoGrosso2122 <- soja %>% filter(UF == 'MT' & Safra == '2021/2022')
 #---------------------------------------#---------------------------------------#---------------------------------------#
 
-#Producao de campos de SEMENTE de soja em comparação com as areas dos estados do Brasil
+#Producao de soja por tipo de semente em comparação com as areas dos estados do Brasil
 ggplot(soja, aes(x = UF, y = Area))+geom_col()+scale_y_continuous(n.breaks = 40, expand = expansion(add = c(0, 5000000)))
 #Imagem: Produção Nacional Area plantada x UF
 
-#Producao de campos de SEMENTE de soja em comparação com as areas dos estados do Brasil safra 2021/2022
+#Producao de soja por tipo de semente em comparação com as areas dos estados do Brasil safra 2021/2022
 ggplot(sojaBrasil2122, aes(x = UF, y = Area))+geom_col()+scale_y_continuous(n.breaks = 40, expand = expansion(add = c(0, 5000000)))
 #Imagem: Produção Nacional Area plantada x UF
 
 #---------------------------------------#---------------------------------------#---------------------------------------#
-#Produção de campos de semente de soja, no Brasil, classificados por area de cada categoria plantada.
+#Producao de soja por tipo de semente, no Brasil, classificados por area de cada categoria plantada.
 ggplot(soja, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 5000000)))
 
-#Produção de campos de semente de soja, no Brasil na safra de 2021/2022, classificados por area de cada categoria plantada.
+#Producao de soja por tipo de semente, no Brasil na safra de 2021/2022, classificados por area de cada categoria plantada.
 ggplot(sojaBrasil2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 10, expand = expansion(add = c(0, 500000)))
 #Imagem: Producao Campo semente soja safra 21 22 Brasil
 
-#Produção de campos de semente de soja, no estado do MT na safra de 2021/2022, classificados por area de cada categoria plantada.
+#Producao de soja por tipo de semente, no estado do MT na safra de 2021/2022, classificados por area de cada categoria plantada.
 ggplot(sojaMatoGrosso2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500000)))
 gMt <- ggplot(sojaMatoGrosso2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500000)))
 #Imagem: Producao campo semente soja safra 21 22 Mato Grosso
 
-#Produção de campos de semente de soja, na cidade de Sinop na safra de 2021/2022, classificados por area de cada categoria plantada.
+#Producao de soja por tipo de semente, na cidade de Sinop na safra de 2021/2022, classificados por area de cada categoria plantada.
 ggplot(sojaSinop2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500)))
 gSinop <- ggplot(sojaSinop2122, aes(x = Categoria, y = Area))+geom_col()+scale_y_continuous(n.breaks = 30, expand = expansion(add = c(0, 500)))
 #Imagem: Producao campo semente soja 21 22 Sinop MT
-
-#---------------------------------------#---------------------------------------#---------------------------------------#
-#Grafico verificando a produção de cada Cultivar na cidade de Sinop na safra 2021/2022 separadas por classes
-gSinop+facet_grid( ~ Cultivar)
-#Imagem: Categorias Semente Soja Plantadas SINOP safra 21 22
-
-#Grafico verificando a produção de cada Cultivar na cidade de Sinop na safra 2021/2022 separadas por classes e status
-gSinop+facet_grid(Status ~ Cultivar)
-#Imagem: Cultivares Status Sinop safra 21 22
-
-#Verificando sobre o estato do Mato Grosso
-gMt+facet_grid( ~ Status)
-#---------------------------------------#---------------------------------------#---------------------------------------#
